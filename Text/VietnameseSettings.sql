3ca0d6733a92ea1ede0a45e02f0b61b279f8e6da
Windrist <1020317@vnu.edu.vn>
2020-09-14 01:25:48 +0700

    Fix Bug

---
+++ Text/VietnameseSettings.sql
@@ -0,0 +1,4 @@
+INSERT INTO "Languages" VALUES('vi_VN','Tiếng Việt',null,1,1);
+INSERT INTO "SteamLanguages" VALUES('vietnamese', 'vi_VN');
+INSERT INTO "LanguagePriorities" VALUES('vi_VN', 'vi_VN', 100);
+INSERT INTO "FontStyleSheets" VALUES('vi_VN', 'Civ6_FontStyles_EFIGS.xml', null, 'MinionPro-Medium.otf');
